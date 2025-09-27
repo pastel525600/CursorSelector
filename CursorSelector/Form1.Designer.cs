d1a7af49444201525b5ed5a0091c1f649fa9937e
pastel525600 <pastel525600@gmail.com>
2025-09-27 17:33:48 +0900

    Add files via upload

---
+++ CursorSelector/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace CursorSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBoxAvailable = new System.Windows.Forms.ListBox();
+            this.numericUpDownInterval = new System.Windows.Forms.NumericUpDown();
+            this.buttonSelectFolder = new System.Windows.Forms.Button();
+            this.buttonApplyNow = new System.Windows.Forms.Button();
+            this.listBoxSelected = new System.Windows.Forms.ListBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbFolder = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxAvailable
+            // 
+            this.listBoxAvailable.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBoxAvailable.FormattingEnabled = true;
+            this.listBoxAvailable.ItemHeight = 24;
+            this.listBoxAvailable.Location = new System.Drawing.Point(2, 36);
+            this.listBoxAvailable.Name = "listBoxAvailable";
+            this.listBoxAvailable.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listBoxAvailable.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxAvailable.Size = new System.Drawing.Size(394, 364);
+            this.listBoxAvailable.TabIndex = 0;
+            // 
+            // numericUpDownInterval
+            // 
+            this.numericUpDownInterval.Location = new System.Drawing.Point(402, 417);
+            this.numericUpDownInterval.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownInterval.Name = "numericUpDownInterval";
+            this.numericUpDownInterval.Size = new System.Drawing.Size(116, 21);
+            this.numericUpDownInterval.TabIndex = 1;
+            this.numericUpDownInterval.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonSelectFolder
+            // 
+            this.buttonSelectFolder.Location = new System.Drawing.Point(12, 10);
+            this.buttonSelectFolder.Name = "buttonSelectFolder";
+            this.buttonSelectFolder.Size = new System.Drawing.Size(100, 20);
+            this.buttonSelectFolder.TabIndex = 2;
+            this.buttonSelectFolder.Text = "폴더 설정";
+            this.buttonSelectFolder.UseVisualStyleBackColor = true;
+            this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+            // 
+            // buttonApplyNow
+            // 
+            this.buttonApplyNow.Location = new System.Drawing.Point(2, 422);
+            this.buttonApplyNow.Name = "buttonApplyNow";
+            this.buttonApplyNow.Size = new System.Drawing.Size(120, 23);
+            this.buttonApplyNow.TabIndex = 3;
+            this.buttonApplyNow.Text = "선택 테마 적용";
+            this.buttonApplyNow.UseVisualStyleBackColor = true;
+            this.buttonApplyNow.Click += new System.EventHandler(this.buttonApplyNow_Click);
+            // 
+            // listBoxSelected
+            // 
+            this.listBoxSelected.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBoxSelected.FormattingEnabled = true;
+            this.listBoxSelected.ItemHeight = 24;
+            this.listBoxSelected.Location = new System.Drawing.Point(402, 36);
+            this.listBoxSelected.Name = "listBoxSelected";
+            this.listBoxSelected.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listBoxSelected.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBoxSelected.Size = new System.Drawing.Size(394, 364);
+            this.listBoxSelected.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(336, 10);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(60, 20);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "추가";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(728, 10);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(60, 20);
+            this.buttonRemove.TabIndex = 6;
+            this.buttonRemove.Text = "삭제";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(662, 418);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(60, 20);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "시작";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(728, 418);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(60, 20);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "정지";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(524, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "초 마다 전환";
+            // 
+            // lbFolder
+            // 
+            this.lbFolder.AutoSize = true;
+            this.lbFolder.Location = new System.Drawing.Point(118, 14);
+            this.lbFolder.Name = "lbFolder";
+            this.lbFolder.Size = new System.Drawing.Size(74, 12);
+            this.lbFolder.TabIndex = 10;
+            this.lbFolder.Text = "C:\\Cursors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.listBoxSelected);
+            this.Controls.Add(this.buttonApplyNow);
+            this.Controls.Add(this.buttonSelectFolder);
+            this.Controls.Add(this.numericUpDownInterval);
+            this.Controls.Add(this.listBoxAvailable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownInterval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxAvailable;
+        private System.Windows.Forms.NumericUpDown numericUpDownInterval;
+        private System.Windows.Forms.Button buttonSelectFolder;
+        private System.Windows.Forms.Button buttonApplyNow;
+        private System.Windows.Forms.ListBox listBoxSelected;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbFolder;
+    }
+}
+
---
+++ CursorSelector/Form1.cs
@@ -0,0 +1,315 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Xml;
+using Timer = System.Threading.Timer;
+
+
+namespace CursorSelector
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        const int SPI_SETCURSORS = 0x57;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDCHANGE = 0x02;
+
+        private string appDataFolder = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "CursorSelector");
+        private string cursorFolder = @"C:\Cursors";
+        private string[] availableThemes;
+        private string[] selectedThemes;
+        private int currentIndex = 0;
+        private Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadAvailableThemes();
+            notifyIcon1.Visible = false; // 트레이 아이콘 초기 비활성
+        }
+
+        // 폴더 선택 및 하위 테마 로드
+        private void buttonSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    cursorFolder = fbd.SelectedPath;
+                    LoadAvailableThemes();
+                    lbFolder.Text = cursorFolder;
+                }
+            }
+        }
+
+        private void LoadAvailableThemes()
+        {
+            if (!Directory.Exists(cursorFolder)) return;
+
+            availableThemes = Directory.GetDirectories(cursorFolder);
+            listBoxAvailable.Items.Clear();
+            foreach (var t in availableThemes)
+                listBoxAvailable.Items.Add(Path.GetFileName(t));
+        }
+
+        // 선택/삭제 → 순서 지정
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBoxAvailable.SelectedItems)
+                listBoxSelected.Items.Add(item);
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            // 선택된 인덱스를 복사
+            int[] indices = new int[listBoxSelected.SelectedIndices.Count];
+            listBoxSelected.SelectedIndices.CopyTo(indices, 0);
+
+            // 내림차순으로 제거
+            Array.Sort(indices);
+            Array.Reverse(indices);
+
+            foreach (int idx in indices)
+                listBoxSelected.Items.RemoveAt(idx);
+        }
+
+
+        // Apply Now
+        private void buttonApplyNow_Click(object sender, EventArgs e)
+        {
+            // 정확히 하나만 선택된 경우만 실행
+            if (listBoxAvailable.SelectedItems.Count == 1)
+            {
+                string themeName = listBoxAvailable.SelectedItem.ToString();
+                string themePath = Path.Combine(cursorFolder, themeName);
+                ApplyCursor(themePath);
+            }
+            else
+            {
+                MessageBox.Show("적용할 테마를 하나만 선택하세요.",
+                                "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        // Start 버튼 → 타이머 반복
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+
+            selectedThemes = new string[listBoxSelected.Items.Count];
+            listBoxSelected.Items.CopyTo(selectedThemes, 0);
+            if (selectedThemes.Length == 0) return;
+            currentIndex = 0;
+
+            buttonSelectFolder.Enabled = false;
+            listBoxAvailable.Enabled = false;
+            listBoxSelected.Enabled = false;
+            buttonApplyNow.Enabled = false; 
+            buttonAdd.Enabled = false;
+            buttonRemove.Enabled = false;
+            numericUpDownInterval.Enabled = false;
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+
+            int interval = (int)numericUpDownInterval.Value * 1000;
+
+            timer = new Timer(_ =>
+            {
+                if (selectedThemes.Length == 0) return;
+
+                string themeName = selectedThemes[currentIndex];
+                string themePath = Path.Combine(cursorFolder, selectedThemes[currentIndex]);
+                ApplyCursor(themePath);
+
+                // ★ 현재 적용 테마를 리스트에서 선택 표시
+                this.Invoke((MethodInvoker)delegate
+                {
+                    listBoxSelected.SelectedIndices.Clear();
+                    int idx = listBoxSelected.Items.IndexOf(themeName);
+                    if (idx >= 0)
+                        listBoxSelected.SelectedIndex = idx;
+                });
+
+                currentIndex++;
+                if (currentIndex >= selectedThemes.Length)
+                    currentIndex = 0;
+
+            }, null, 0, interval);
+            
+            StartWithWindows();
+
+            string configPath = Path.Combine(appDataFolder, "config.txt");
+            var lines = new List<string>
+            {
+                $"CursorFolder={cursorFolder}",
+                $"SelectedThemes={string.Join(",", listBoxSelected.Items.Cast<string>())}",
+                $"IntervalSec={numericUpDownInterval.Value}",
+                $"AutoStart={true}" 
+            };
+            File.WriteAllLines(configPath, lines);
+
+            // 트레이 최소화
+            notifyIcon1.Visible = true;
+            this.Hide();
+        }
+
+        // Stop 버튼 → 반복 종료
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            StopTimer();
+
+            string configPath = Path.Combine(appDataFolder, "config.txt");
+            var lines = new List<string>
+            {
+                $"CursorFolder={cursorFolder}",
+                $"SelectedThemes={string.Join(",", listBoxSelected.Items.Cast<string>())}",
+                $"IntervalSec={numericUpDownInterval.Value}",
+                $"AutoStart={false}"
+            };
+            File.WriteAllLines(configPath, lines);
+
+            buttonSelectFolder.Enabled = true;
+            listBoxAvailable.Enabled = true;
+            listBoxSelected.Enabled = true;
+            buttonApplyNow.Enabled = true;
+            buttonAdd.Enabled = true;
+            buttonRemove.Enabled = true;
+            numericUpDownInterval.Enabled = true;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void StopTimer()
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+                timer = null;
+            }
+        }
+
+        // 커서 적용
+        private void ApplyCursor(string themeFolder)
+        {
+            var cursorMap = new (string key, string file)[]
+            {
+                ( "Arrow", "Normal Select.ani" ),
+                ( "Hand", "Link Select.ani" ),
+                ( "IBeam", "Text Select.ani" ),
+                ( "Crosshair", "Precision Select.ani" ),
+                ( "Help", "Help Select.ani" ),
+                ( "No", "Unavailable.ani" ),
+                ( "AppStarting", "Working In Background.ani" ),
+                ( "UpArrow", "Alternate Select.ani" ),
+                ( "SizeAll", "Move.ani" ),
+                ( "SizeNESW", "Diagonal Resize 2.ani" ),
+                ( "SizeNS", "Vertical Resize.ani" ),
+                ( "SizeNWSE", "Diagonal Resize 1.ani" ),
+                ( "SizeWE", "Horizontal Resize.ani" ),
+                ( "Pin", "Location Select.ani" ),
+                ( "Person", "Person Select.ani" ),
+                ( "NWPen", "Handwriting.ani" ),
+                ( "Wait", "Busy.ani" )
+            };
+
+            // 레지스트리에 적용
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Cursors", true))
+            {
+                foreach (var (k, f) in cursorMap)
+                {
+                    string path = Path.Combine(themeFolder, f);
+                    if (File.Exists(path))
+                        key.SetValue(k, path);
+                }
+            }
+
+            // 시스템에 적용
+            SystemParametersInfo(SPI_SETCURSORS, 0, null, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+        }
+
+        // 폼 닫기 → 트레이로 최소화
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        // Windows 시작 시 자동 실행 등록
+        private void StartWithWindows()
+        {
+            string targetExePath = Path.Combine(appDataFolder, "CursorSelector.exe");
+            string currentExe = Application.ExecutablePath;
+
+            if (!File.Exists(targetExePath))
+                File.Copy(currentExe, targetExePath);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Run", true);
+
+            key.SetValue("CursorSelector", targetExePath);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(appDataFolder))
+                Directory.CreateDirectory(appDataFolder);
+
+            string configPath = Path.Combine(appDataFolder, "config.txt");
+            if (File.Exists(configPath))
+            {
+                var lines = File.ReadAllLines(configPath);
+                var config = lines
+                    .Select(l => l.Split(new[] { '=' }, 2))
+                    .ToDictionary(s => s[0], s => s[1]);
+
+                // 값 적용
+                cursorFolder = config.ContainsKey("CursorFolder") ? config["CursorFolder"] : "";
+                LoadAvailableThemes();
+
+                if (config.ContainsKey("SelectedThemes"))
+                {
+                    // SelectedThemes는 ','로 구분
+                    var themes = config["SelectedThemes"].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                    listBoxSelected.Items.AddRange(themes);
+                }
+
+                if (config.ContainsKey("IntervalSec") && int.TryParse(config["IntervalSec"], out int interval))
+                {
+                    numericUpDownInterval.Value = interval;
+                }
+
+                if (config.ContainsKey("AutoStart") && bool.TryParse(config["AutoStart"], out bool autoStart) && autoStart)
+                {
+                    // 바로 타이머 시작 & 트레이로
+                    buttonStart_Click(this, new EventArgs());
+                }
+            }
+            this.Hide();
+            notifyIcon1.Visible = true;
+        }
+
+        public class AppConfig
+        {
+            public string CursorFolder { get; set; }
+            public string[] SelectedThemes { get; set; }
+            public int IntervalSec { get; set; }
+            public bool AutoStart { get; set; }
+        }
+    }
+}
