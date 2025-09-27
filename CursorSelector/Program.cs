bfdd2c5ba15309222ea3e4af2de039bcb3248b21
pastel525600 <pastel525600@gmail.com>
2025-09-27 18:19:32 +0900

    Add files via upload

--- CursorSelector/Program.cs
+++ CursorSelector/Program.cs
@@ -13,7 +13,21 @@ namespace CursorSelector
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            if (IsAlreadyRunning())
+            {
+                MessageBox.Show("이미 프로그램이 실행 중입니다.",
+                                "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
             Application.Run(new Form1());
         }
+        private static bool IsAlreadyRunning()
+        {
+            var processes = System.Diagnostics.Process.GetProcessesByName(
+                System.IO.Path.GetFileNameWithoutExtension(
+                    System.Reflection.Assembly.GetEntryAssembly().Location));
+            return processes.Length > 1;
+        }
     }
+
 }
